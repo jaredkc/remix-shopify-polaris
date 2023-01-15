19d63151d9dde4aede66977c64f5d1fb86a8cf23
jaredkc <jared@clivecoffee.com>
2023-01-14 16:53:37 -0800

    example pages

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,17 @@
+import { Card, EmptyState, Page } from '@shopify/polaris';
+import AppFrame from '~/components/AppFrame';
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <AppFrame>
+      <Page title="Remix with Shopify Polaris">
+        <Card sectioned>
+          <EmptyState
+            heading="Add your dashboard here"
+            image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+          />
+        </Card>
+      </Page>
+    </AppFrame>
   );
 }
--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -3,17 +3,15 @@ import AppFrame from '~/components/AppFrame';
 
 export default function Settings() {
   return (
-    <div>
-      <AppFrame>
-        <Page title="Settings">
-          <Card sectioned>
-            <EmptyState
-              heading="Manage your settings"
-              image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
-            />
-          </Card>
-        </Page>
-      </AppFrame>
-    </div>
+    <AppFrame>
+      <Page title="Settings">
+        <Card sectioned>
+          <EmptyState
+            heading="Manage your settings"
+            image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+          />
+        </Card>
+      </Page>
+    </AppFrame>
   );
 }
