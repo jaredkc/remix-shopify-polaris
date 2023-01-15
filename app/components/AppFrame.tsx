3fd1ac27c30630f04cf30d9205ad4a562ea177f4
jaredkc <jared@clivecoffee.com>
2023-01-14 16:53:31 -0800

    component with Polaris app frame, logo and Remix navigation

---
+++ app/components/AppFrame.tsx
@@ -0,0 +1,88 @@
+import { useMatches } from '@remix-run/react';
+import { Frame, Navigation, TopBar } from '@shopify/polaris';
+import { useCallback, useState } from 'react';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const AppFrame = ({ children }: Props) => {
+  const [userMenuActive, setUserMenuActive] = useState(false);
+  const [mobileNavigationActive, setMobileNavigationActive] = useState(false);
+  const matches = useMatches();
+  const {pathname} = matches[matches.length - 1];
+  console.log('matches', pathname);
+
+  const toggleUserMenuActive = useCallback(
+    () => setUserMenuActive((userMenuActive) => !userMenuActive),
+    []
+  );
+  const toggleMobileNavigationActive = useCallback(
+    () =>
+      setMobileNavigationActive(
+        (mobileNavigationActive) => !mobileNavigationActive
+      ),
+    []
+  );
+
+  const userMenuActions = [
+    {
+      items: [{ content: 'hello@example.com' }],
+    },
+  ];
+
+  const userMenuMarkup = (
+    <TopBar.UserMenu
+      actions={userMenuActions}
+      name={'Username'}
+      initials={'U'}
+      open={userMenuActive}
+      onToggle={toggleUserMenuActive}
+    />
+  );
+
+  const topBarMarkup = (
+    <TopBar
+      showNavigationToggle
+      userMenu={userMenuMarkup}
+      onNavigationToggle={toggleMobileNavigationActive}
+    />
+  );
+
+  const nav = [
+    { label: 'Orders', url: '/orders' },
+    { label: 'Product', url: '/products' },
+    { label: 'Settings', url: '/settings' },
+    { label: 'Statements', url: '/statements' },
+  ];
+
+  const navigationMarkup = (
+    <Navigation location={pathname}>
+      <Navigation.Section items={nav} />
+    </Navigation>
+  );
+
+  const logo = {
+    width: 200,
+    topBarSource: '/app-logo.svg',
+    contextualSaveBarSource: '/app-logo-light.svg',
+    url: '/',
+    accessibilityLabel: 'Remix with Shopify Polaris',
+  };
+
+  return (
+    <div>
+      <Frame
+        logo={logo}
+        topBar={topBarMarkup}
+        navigation={navigationMarkup}
+        showMobileNavigation={mobileNavigationActive}
+        onNavigationDismiss={toggleMobileNavigationActive}
+      >
+        {children}
+      </Frame>
+    </div>
+  );
+};
+
+export default AppFrame;
